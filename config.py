37632e44a3e8f9c32318467affd36f96194afab1
Venkat3250 <113410560+Venkat3250@users.noreply.github.com>
2026-01-05 21:46:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-MESSAGE_TEXT = "Hi, thanks for connecting! I’m exploring opportunities and would love to know if there are any openings or referrals in your organization."
-
-EMAIL_SENDER = "your_email@gmail.com"
-EMAIL_RECEIVER = "your_email@gmail.com"
+MESSAGE_TEXT = "Hi, thanks for connecting.I’m exploring opportunities within your organization.As part of my hands-on learning, I’ve built and deployed an end-to-end automation workflow to practice professional outreach — this message is sent via that system as a skills demonstration.I’m focused on backend automation and cloud workflows, and I’d be happy to share details if helpful.I’d appreciate your guidance on any suitable roles or teams. Thanks!"
+EMAIL_SENDER = "mohan38719@gmail.com"
+EMAIL_RECEIVER = "mohan38719@gmail.com"
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 EMAIL_SUBJECT = "LinkedIn Bot Notification"
